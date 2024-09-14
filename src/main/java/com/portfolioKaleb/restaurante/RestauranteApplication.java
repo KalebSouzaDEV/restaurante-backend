fd12113203f7c130d3fee8e2ed3a7c985534e729
Kaleb Souza <88kaleb88@gmail.com>
2024-09-14 01:47:34 -0300

    Deploy Railway

--- src/main/java/com/portfolioKaleb/restaurante/RestauranteApplication.java
+++ src/main/java/com/portfolioKaleb/restaurante/RestauranteApplication.java
@@ -13,9 +13,11 @@ public class RestauranteApplication {
 
 	public static void main(String[] args) {
 		Flyway flyway = Flyway.configure()
-				.dataSource("jdbc:postgresql://junction.proxy.rlwy.net:30153/railway", "postgres", "OTLFCJYPNZFdsuHctuDajGlBNXmFcbnM")
+				.dataSource("jdbc:postgresql://junction.proxy.rlwy.net:30153/railway\n", "postgres", "OTLFCJYPNZFdsuHctuDajGlBNXmFcbnM")
+				.cleanDisabled(false)
 				.baselineOnMigrate(true)
 				.load();
+		flyway.clean();
 		flyway.migrate();
 		SpringApplication.run(RestauranteApplication.class, args);
 	}
