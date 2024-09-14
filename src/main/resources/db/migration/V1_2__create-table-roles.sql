5233f1ef067c353dd5301bc21d9d00937b255057
Kaleb Souza <88kaleb88@gmail.com>
2024-09-14 12:52:13 -0300

    Deploy Railway

---
+++ src/main/resources/db/migration/V1_2__create-table-roles.sql
@@ -0,0 +1,7 @@
+CREATE TABLE roles (
+    role_id INT,
+    name TEXT
+);
+
+INSERT INTO roles VALUES (1, 'CLIENT');
+INSERT INTO roles VALUES (2, 'ADMIN')
---
+++ src/main/resources/db/migration/V1_5__modify-table-users.sql
@@ -0,0 +1,3 @@
+ALTER TABLE users ADD nome TEXT;
+ALTER TABLE users ADD phone TEXT;
+ALTER TABLE users ADD address TEXT;
