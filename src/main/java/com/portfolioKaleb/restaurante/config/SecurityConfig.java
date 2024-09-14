995351aaf3f84b7e0bd45cb84e098f6e167588d6
Kaleb Souza <88kaleb88@gmail.com>
2024-09-14 13:25:23 -0300

    Deploy Railway

--- src/main/java/com/portfolioKaleb/restaurante/config/SecurityConfig.java
+++ src/main/java/com/portfolioKaleb/restaurante/config/SecurityConfig.java
@@ -57,20 +57,14 @@ public class SecurityConfig {
     @PostConstruct
     public void init() throws Exception {
         System.out.println("VAI SI FUDERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR: \n" + publicKeyResource + "\n " + privateKeyResource + "\n VAI SI FUDERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
-        this.publicKey = loadPublicKey(publicKeyResource);
-        this.privateKey = loadPrivateKey(privateKeyResource);
+        this.publicKey = loadPublicKey();
+        this.privateKey = loadPrivateKey();
         System.out.println("TROCARIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA:: \n" + this.publicKey  + "\n" + this.privateKey);
     }
 
-    private RSAPublicKey loadPublicKey(Resource resource) throws Exception {
+    private RSAPublicKey loadPublicKey() throws Exception {
         System.out.println("public1");
-        String key = new String(Files.readAllBytes(resource.getFile().toPath()));
-        key = key.replaceAll("-----BEGIN PUBLIC KEY-----", "")
-                .replaceAll("-----END PUBLIC KEY-----", "")
-                .replaceAll("\\s", ""); // Remove newlines and spaces
-        System.out.println("public2" + key);
-
-        byte[] keyBytes = Base64.getDecoder().decode(key);
+        byte[] keyBytes = Base64.getDecoder().decode("MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCZnRv5SRhz8c4AsbeDiOl6f3QnATty+I2yaJ4eSt3dYJ80BzGz511vQI/cVGmp4X6wVyQeiwyvBsHS7FOjIHD/rK+El9urhNc10YbPRa+RTrBg6I+XkDunwEJtFfevgA6YM4GVSunC/mVLXYLcee/me+BPRGYn1DZoeNG/l/YhdTuEZLop7bxMvVAqdtw/BzFZo7q1IJlMQQlp5Gmv8+rVDOjqxfvVj36g3EZZ/EOqzT99cIv2U9eFS+StMk7gSfSv71qLw9Vgblx5m5tv8wNjepSPkThksw8/whI5gEZRXE+A+/QYzjQk6rEuSoaA3zBaHlswRwuTk0YUH5OJlvMVAgMBAAECggEAJgS4U64/mxKRhaJ2ZNuRzXuV58FqUT4q86jo2V52IsAzJtFQ/9GXUl2MPV+29xCUJliq29RWQ0tM1UMgQBNbZCv9FLfw6jZqfStqTgpVNYVgKdvlDgHogdAXoJ/7OThcZFb7D1QHHvePYmO2a5LSSR0Hmxa2oczn2ltsrhqSFB1pfwa3YpLqf/jXVj2qJGLw8Jq7/QW5ad53Do9NB83SaJb58WVhAFSoPpMU51sS/GwGGX04j57MoA6SqRXFnL0TKAOJ2AMSoc72SCder1+BjyKjHI6lOt3CqIWOnqEDw8Xsw+cnl1K9Y1vU73KxReAW6OOg4DXOUofdUQUcNZdUUQKBgQDIevUWK8MG7ZXm5A2zSkRwcH6xnh5klrgeJDlUGqooHXbpv9++rWWexdEh1W6bM7KnrNIC+JWQp8QPaIpKagURXruqvxiWmaXjN1cBSmxNOfGp5/lppUovyRKPVuJeVPnGtTDHaLXoXqmJiiMkT6VQJZZaIbGIbYafjdqh+78GxwKBgQDEJ4vy4yxVPUMzP6PNsrUoxRn6YkxIyp9xSPohyCCgdijNOW4v73HmHinVP61ADZAa2loxqpvNizgeILo4URroKUKOeRFZ2J/QteZNJT02FsgTg4RVsy2RZYKjVQSFXIIXgi7eLP8IMKz6xAqVgnSTfAUKZy3PR8pdRwkzjCVrQwKBgDZha/O8oSVS8vNzbufwQEpDqYoietoMLBs49jgDF57UBzp2qol9XXBI9mZ4D42X42EqNrlJlsBrUXpggAqZYZCJotw5A8FuIhpVG4ulFWWJTy0oWHY4uLvnjdpF1heCc1JcOcQNejJy4wgE6W1jEVOBLpx6QT7hfr/u6ZxAvltnAoGARRUMc8FQNsD5rs3l+abLqI0qJgWCikxaNQKXnVgnLXwPyJ9GUDmLKplK8GCZuqpMKzjldQJ6FrzGSc4K55Sg3kzRtO2sj7D8L/wwHNFVqnwBgOXbsHcHQGK4zJ8/lVxMMM1XhkXgl9jUwwhSFe+48MxH1i8achr9iTZgZjggYZMCgYAJ8SiPgg1EJP+RZfB1O2NgTJQ9JuGwoUxJZldaZdIZuoxHMcyZKXZpj5DDxm5zsTbrCZ30aushFEL8CbsV43lw6jLp5QbpJBYq2GbC6Agqpr0WJo/2e66q4l+kRL3bETh2UcH1RYsKi6IfOlx2NefkdsVsBZA2z+cEOkoPNQfkTA==");
         System.out.println("public3");
         X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
         System.out.println("public4");
@@ -79,16 +73,9 @@ public class SecurityConfig {
         return (RSAPublicKey) keyFactory.generatePublic(spec);
     }
 
-    private RSAPrivateKey loadPrivateKey(Resource resource) throws Exception {
-        System.out.println("private1");
-        String key = new String(Files.readAllBytes(resource.getFile().toPath()));
-        System.out.println("private2");
-        key = key.replaceAll("-----BEGIN PRIVATE KEY-----", "")
-                .replaceAll("-----END PRIVATE KEY-----", "")
-                .replaceAll("\\s", ""); // Remove newlines and spaces
-        System.out.println("VAI SI FUDER PORRAA2:::::::::::::: " + key);
+    private RSAPrivateKey loadPrivateKey() throws Exception {
         System.out.println("private3");
-        byte[] keyBytes = Base64.getDecoder().decode(key);
+        byte[] keyBytes = Base64.getDecoder().decode("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmZ0b+UkYc/HOALG3g4jpen90JwE7cviNsmieHkrd3WCfNAcxs+ddb0CP3FRpqeF+sFckHosMrwbB0uxToyBw/6yvhJfbq4TXNdGGz0WvkU6wYOiPl5A7p8BCbRX3r4AOmDOBlUrpwv5lS12C3Hnv5nvgT0RmJ9Q2aHjRv5f2IXU7hGS6Ke28TL1QKnbcPwcxWaO6tSCZTEEJaeRpr/Pq1Qzo6sX71Y9+oNxGWfxDqs0/fXCL9lPXhUvkrTJO4En0r+9ai8PVYG5ceZubb/MDY3qUj5E4ZLMPP8ISOYBGUVxPgPv0GM40JOqxLkqGgN8wWh5bMEcLk5NGFB+TiZbzFQIDAQAB");
         System.out.println("private4");
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
         System.out.println("private5");
